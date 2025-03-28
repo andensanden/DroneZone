76d2a462502920b2aaf25c0bb77e6982d011e483
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-03-28 15:59:37 +0100

    feat: version 1 of about us page is done for computer view

---
+++ dronezone/src/componets/aboutInfo.jsx
@@ -0,0 +1,18 @@
+
+
+
+export function AboutInfo() {
+  return (
+    <div className="bg-primary-yellow p-10 w-full grow ">
+      <div className="flex flex-col max-w-5/8">
+            <h2 className="text-3xl font-bold my-4">About</h2>
+            <p className="mb-6">DroneZone is an app for drone enthusiasts and pilots. With DroneZone, users can easily register and track their drones, ensuring safety and efficiency. Plan your flights by drawing custom flight paths and get real-time updates on drone locations. Stay in control and keep your drone flying safely with this intuitive and user-friendly app.
+            </p>
+            <p>
+              We're are a team of seven students from KTH Royal Institute of Technology. If you'd like to learn more, <span><a href="https://github.com/II1305" target="_blank" className="text-primary-light-blue underline">click here</a></span>
+            </p>
+        </div>
+    </div>
+  )
+}
+
--- dronezone/src/pages/AboutUs.jsx
+++ dronezone/src/pages/AboutUs.jsx
@@ -1,10 +1,14 @@
-import { Footer } from "@/componets/footer";
+
 import { Header } from "@/componets/header";
+import { AboutInfo } from "@/componets/aboutInfo";
+import { Footer } from "@/componets/footer";
+
 
 export function AboutUs() {
   return (
-    <div>
+    <div className="h-screen flex flex-col">
       <Header />
+      <AboutInfo />
       <Footer />
     </div>
   )
