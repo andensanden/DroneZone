95628d7f0d08843b5a06d9801c5b0e6f98d273eb
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-03-28 16:01:50 +0100

    feat: version 1 of about us page is done for computer view

--- dronezone/src/pages/Home.jsx
+++ dronezone/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import { Header } from "@/componets/header";
 import { Map } from "@/componets/map";
-import { Footer } from "@/componets/footer";
 
 export function Home() {
   return (
     <div>
       <Header />
       <Map />
-      <Footer />
     </div>
   )
 }
