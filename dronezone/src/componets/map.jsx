c05c082099f286114a128bfd0d2b1f398162ceee
Rasmus Sundbom <rasmus@driftgruppen.se>
2025-04-02 15:46:24 +0200

    Update start position

--- dronezone/src/componets/map.jsx
+++ dronezone/src/componets/map.jsx
@@ -1,10 +1,10 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 //Gelocation api för start coords ~~~
 
 const Map = () => {
-  const position = [51.505, -0.09]; // Coordinates for the map's center
+  const position = [59.3293, 18.0686]; // Coordinates for the map's center
 
   return (
     <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
@@ -12,11 +12,6 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 };
