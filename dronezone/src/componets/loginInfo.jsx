1124b0e60d84df716ff120aea7f71e69669ca273
ref <ref@kth.se>
2025-04-02 10:30:40 +0200

    login

---
+++ dronezone/src/componets/loginInfo.jsx
@@ -0,0 +1,19 @@
+import { Logo } from "@/componets/logo";
+
+export function LoginInfo() {
+    return (
+      <div >
+        <div className="min-h-screen flex flex-col items-center justify-center">
+              <div className="w-100 h-100">
+              <Logo />
+          </div>
+          <div className="flex items-center justify-center p-10">
+  
+  </div>
+</div>
+
+<button className="text-gray-700 bg-transparent ml-2 font-bold text-sm p-2 hover:underline w-16" onClick={() => navigate('/')}>Hej</button>
+      </div>
+    )
+  }
+  /*className="flex items-center mr-10 h-12 bg-gray-200 rounded-2xl"*/
