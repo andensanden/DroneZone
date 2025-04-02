feb5718060226c443e5d28128f2ead3bac627294
Rasmus Sundbom <rasmus@driftgruppen.se>
2025-04-02 16:49:30 +0200

    Add GPS to map

--- dronezone/src/componets/map.jsx
+++ dronezone/src/componets/map.jsx
@@ -1,10 +1,32 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-//Gelocation api för start coords ~~~
+
+/*
+  Clicking on the map causes the map to zoom to your location and place a marker
+*/
+function LocationMarker() {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
 
 const Map = () => {
-  const position = [59.3293, 18.0686]; // Coordinates for the map's center
+  const position = [59.3293, 18.0686]; // Coordinates for the map's center 
 
   return (
     <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
@@ -12,6 +34,7 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <LocationMarker />
     </MapContainer>
   );
 };
