653c42dcc3d9a142d78920e9142623f98075a0ce
Rasmus Sundbom <rasmus@driftgruppen.se>
2025-04-03 11:55:15 +0200

    Add buffer zone to paths

--- dronezone/src/mapScripts/pathDrawing.jsx
+++ dronezone/src/mapScripts/pathDrawing.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useMap, Circle, Polyline } from 'react-leaflet'
+import { useMap, Circle, Polyline, Polygon } from 'react-leaflet'
 
 const nodeRadius = 20;
 
@@ -10,6 +10,7 @@ function MapClick() {
     const map = useMap()
     const [nodes, setNodes] = useState([])
     const [paths, setPaths] = useState([])
+    const [bufferZones, setBufferZones] = useState([])
 
     const onMapClick = (e) => {
         AddNode(e, setNodes)
@@ -19,6 +20,7 @@ function MapClick() {
     useEffect(() => {
         if (nodes.length > 1) {
             AddPath(nodes[nodes.length-2], nodes[nodes.length-1], setPaths)
+            AddBufferZone(nodes[nodes.length-2], nodes[nodes.length-1], setBufferZones)
         }
     }, [nodes])
 
@@ -34,6 +36,7 @@ function MapClick() {
         <>
             <DrawNodes nodes={nodes}/>
             <DrawPaths paths={paths}/>
+            <DrawBufferZones bufferZones={bufferZones}/>
         </>
     )
 }
@@ -87,6 +90,68 @@ function DrawPaths({paths}) {
             })}
         </>
     )
+}
+
+/*
+    Add a new buffer zone to the buffer zone array
+*/
+function AddBufferZone(startNode, endNode, setBufferZones) {
+    const bufferWidth = 40;
+    const newZone = CreateBufferCoords([startNode.position, endNode.position], bufferWidth)
+    setBufferZones((prevZones) => [...prevZones, newZone])
+}
+
+/*
+    Draws the existing buffer zones on the map
+*/
+function DrawBufferZones({bufferZones}) {
+    return (
+        <>
+            {bufferZones.map((zone, index) => { return(
+                <Polygon 
+                key={index} positions={zone}
+                color="blue" fillOpacity={0.3} weight={1}/>
+                )
+            })}
+        </>
+    )
+}
+
+/*
+    Calculates the offset of the buffer zone coordinates from the path between the nodes.
+    These coordinates are used in AddBufferZone to create the polygon.
+*/
+function CreateBufferCoords(coords, widthMeters) {
+    if (coords.length < 2) return [];
+    
+    const halfWidth = widthMeters / 2 / 111320; // Approx meters to degrees
+    let leftSide = [];
+    let rightSide = [];
+    
+    const p1 = coords[0];
+    const p2 = coords[1];
+    const angle = Math.atan2(p2.lat - p1.lat, p2.lng - p1.lng);
+    const perpAngle = angle + Math.PI/2;
+    
+    // Calculate offset points
+    leftSide.push([
+        p1.lat + Math.sin(perpAngle) * halfWidth,
+        p1.lng + Math.cos(perpAngle) * halfWidth
+    ]);
+    rightSide.push([
+        p1.lat - Math.sin(perpAngle) * halfWidth,
+        p1.lng - Math.cos(perpAngle) * halfWidth
+    ]);
+    leftSide.push([
+        p2.lat + Math.sin(perpAngle) * halfWidth,
+        p2.lng + Math.cos(perpAngle) * halfWidth
+    ]);
+    rightSide.push([
+        p2.lat - Math.sin(perpAngle) * halfWidth,
+        p2.lng - Math.cos(perpAngle) * halfWidth
+    ]);
+    
+    return leftSide.concat(rightSide.reverse());
 }
 
 export default MapClick
