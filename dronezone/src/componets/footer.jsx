6ba6a6c53e758e0f49c49b84b1c0017ba1859d82
Malin Andréasson <malandre@kth.se>
2025-04-04 14:15:53 +0200

    fixed mail missspelling

--- dronezone/src/componets/footer.jsx
+++ dronezone/src/componets/footer.jsx
@@ -7,7 +7,7 @@ export function Footer() {
             <div className="flex flex-col">
                 <h2 className="font-bold text-xl">Contact</h2>
                 <p className="text-sm text-gray-400">
-                <a href="mailto:teameliza@patron.me"> teameliza@patron.me </a>
+                <a href="mailto:teameliza@proton.me"> teameliza@proton.me </a>
                 </p>
             </div>
             <div className="flex flex-col">
