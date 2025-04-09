108d756bdfdd67df2be63a56936b2623740030d0
rehrnroooth <rebecca.from1@gmail.com>
2025-04-09 16:53:18 +0200

    logged in map update

--- dronezone/src/pages/Home.jsx
+++ dronezone/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/header";
 import Map from "@/components/map";
 import { Footer } from "@/components/footer";
+import LoggedInMap from "@/components/loggedInMap";
 
 export function Home() {
   return (
     <>
       <Header />
-      <Map />
+      <LoggedInMap />
       <Footer />
     </>
   );
