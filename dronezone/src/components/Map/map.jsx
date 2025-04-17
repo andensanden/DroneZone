112110a6ae7f50c7e82abfa9f78317675787b3fb
Malin Andréasson <malandre@kth.se>
2025-04-17 13:19:31 +0200

    checking if the marker issue is resolved

--- dronezone/src/components/Map/map.jsx
+++ dronezone/src/components/Map/map.jsx
@@ -25,6 +25,23 @@ import LocationTracker from "@/mapScripts/locationTracker";
 import { HamburgerButton } from "./layerHamburgerMenu";
 import GPSToggleControl from "@/mapScripts/gpsToggleControl";
 
+
+
+/**MARKER ON MAP DEPLOYED VERSION*/
+
+import L from 'leaflet';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
+
 //-------- Main Map Component -------
 const Map = () => {
   const [trackingEnabled, setTrackingEnabled] = useState(true);
