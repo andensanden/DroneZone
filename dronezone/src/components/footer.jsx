72eac44417eb6ad2b56494e86ed68af0f1206a2c
Malin Andréasson <malandre@kth.se>
2025-05-08 10:16:49 +0200

    footer is smallscreen compatible

--- dronezone/src/components/footer.jsx
+++ dronezone/src/components/footer.jsx
@@ -2,17 +2,17 @@ import { Logo } from "@/components/logo";
 
 export function Footer() {
   return (
-    <div className="flex items-center justify-center relative border-t-1 border-gray-200">
-      <div className="flex flex-col basis-1  sm:flex-row gap-30 sm:basis-1/2 flex-grow p-16">
+    <div className="flex items-start lg:items-center justify-center relative border-t-1 border-gray-200">
+      <div className="flex flex-col basis-1 gap-10 p-10 lg:flex-row lg:gap-30 lg:basis-1/2 flex-grow lg:p-16">
         <div className="flex flex-col">
           <h2 className="font-bold text-xl">Contact</h2>
-          <p className="text-sm text-gray-400">
+          <p className="text-lg text-gray-400">
             <a href="mailto:teameliza@proton.me"> teameliza@proton.me </a>
           </p>
         </div>
         <div className="flex flex-col">
           <h2 className="font-bold text-xl">Terms and conditions</h2>
-          <p className="text-sm text-gray-400">
+          <p className="text-lg text-gray-400">
             This website is operated by a student team from KTH as part of a
             course project. By using the site, you agree to comply with Swedish
             law and our terms of use. Personal data is handled in accordance
@@ -20,7 +20,7 @@ export function Footer() {
           </p>
         </div>
       </div>
-      <div className="flex p-5 items-start sm:items-center justify-end p-10 ">
+      <div className="hidden sm:flex sm:items-start sm:justify-end lg:items-center p-10 ">
         <Logo />
       </div>
     </div>
