c824f754a4c9b62867801176a789ca90fffe504a
Malin Andreasson <malandre@kth.se>
2025-05-14 11:31:05 +0200

    malins feedback fixes (#83)
    * malins feedback fixes
    
    * crisis fix

--- dronezone/src/components/About/aboutInfo.jsx
+++ dronezone/src/components/About/aboutInfo.jsx
@@ -9,7 +9,7 @@ export function AboutInfo() {
             <p className="mb-6">DroneZone is an app for drone enthusiasts and pilots. With DroneZone, users can easily register and track their drones, ensuring safety and efficiency. Plan your flights by drawing custom flight paths and get real-time updates on drone locations. Stay in control and keep your drone flying safely with this intuitive and user-friendly app.
             </p>
             <p>
-              We're are a team of seven students from KTH Royal Institute of Technology. If you'd like to learn more, <span><a href="https://team-eliza-webpage.vercel.app/" target="_blank" className="text-primary-light-blue underline">click here</a></span>
+              We're are a team of seven students from KTH Royal Institute of Technology. If you'd like to learn more, <span><a href="https://team-eliza-webpage.vercel.app/" target="_blank" className="text-primary-light-blue underline">click here</a></span>.
             </p>
         </div>
     </div>
--- dronezone/src/components/ui/header.jsx
+++ dronezone/src/components/ui/header.jsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router";
 import { GrLogin } from "react-icons/gr";
 import { useSelector, useDispatch } from "react-redux";
-import { LoginDropDown } from "@/components/ui/loginDropDown";
-import { LuInbox } from "react-icons/lu";
+import { LoginDropDown } from "./loginDropDown";
 import { GoQuestion } from "react-icons/go";
 import { NavigationMenu } from "../Navigation/navigationMenu";
+import { FaUser } from "react-icons/fa";
+
 
 export function Header() {
   const navigate = useNavigate();
@@ -14,7 +15,8 @@ export function Header() {
   return (
     <div className="bg-white h-25 flex items-center justify-between ">
       <div className="flex flex-col">
-        <h1 className="text-3xl font-bold text-gray-700 ml-8">
+        <h1 className="text-3xl font-bold text-gray-700 ml-8 cursor-default"
+        onClick={() => navigate("/")}>
           Drone<span className="text-yellow-500">Zone</span>
         </h1>
         <p className="hidden lg:block text-gray-400 ml-8 text-sm">
@@ -25,11 +27,7 @@ export function Header() {
       {/*Navigation bar*/}
       <div className="flex items-center gap-4">
         {/*We dont need this NOTIFICATION button*/}
-        {/*x{isAuth ? <div className="relative hover:scale-107 transition-all duration-200">
-          <button className="bg-primary-blue p-3 rounded-xl"><LuInbox size={20} className="text-white"/> </button> 
-        <span className="text-white text-sm font-bold bg-red-600 rounded-full w-4 h-4 flex items-center justify-center absolute top-0 right-0">1</span>
-        </div> : null}*/}
-
+        
         {/* mobile compatible menu */}
         <div className="flex items-center p-3 mr-5 bg-primary-yellow  rounded-xl sm:hidden hover:scale-107 transition-all duration-200">
           <NavigationMenu />
@@ -62,11 +60,15 @@ export function Header() {
             />{" "}
           </button>
 
-          <div className="flex items-center justify-center text-gray-700 bg-primary-yellow h-12 font-bold text-sm rounded-2xl ml-2 p-2 w-16 hover:scale-107 transition-all duration-200">
+          <div className="flex items-center justify-center text-gray-700 bg-primary-yellow h-12 font-bold text-sm rounded-2xl ml-2 p-2 w-16 hover:scale-107 transition-all duration-200"
+               onClick={() =>  {
+                isAuth ? (<LoginDropDown/> ) : (<FaUser size={20}/>)
+              }}
+          >
             {isAuth ? (
               <LoginDropDown />
             ) : (
-              <GrLogin size={20} onClick={() => navigate("/login")} />
+              <FaUser size={20} onClick={() => navigate("/login")} />
             )}
           </div>
         </div>
